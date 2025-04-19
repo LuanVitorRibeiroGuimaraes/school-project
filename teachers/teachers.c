90f97312a12251d8e251054bf9c446ce0aa9f38d
Caio Gabriel <cgabriel.amorim@gmail.com>
2025-04-19 18:36:56 -0300

    Desenvolvimento da entidade professor.

--- teachers/teachers.c
+++ teachers/teachers.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "teachers.h"
+#include "../validators/validators.h"
 
-typedef struct alunos
-{
-    
-};
+#define SIZE_PROFESSORES 100
+
+teacherQnt = 0;
+Teacher teachers[SIZE_PROFESSORES]; 
+
+#pragma region CRIAR PROFESSOR
+void createTeacher(){
+    int teacherId; 
+    char name[100];
+    char sex;
+    int birthDate;
+    long cpf;
+
+    if(teacherQnt == SIZE_PROFESSORES){
+        printf("\nNao eh possivel cadastrar mais alunos!");
+    }
+
+    printf("Digite a matrícula do aluno: ");
+    scanf("%d", &teacherId);
+
+    getchar();
+
+    printf("Digite o nome do professor: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = '\0';
+
+
+    printf("Digite o sexo do professor (F ou M): ");
+    scanf("%c", &sex);
+
+    getchar();
+
+    printf("Digite a data de aiversario: ");
+    scanf("%d", &birthDate);
+
+    getchar();
+
+    printf("Digite o CPF do professor: ");
+    scanf("%ld", &cpf);
+    validateCpf(cpf);
+
+    teachers[teacherQnt].studentId = teacherId;
+    strcpy(teachers[teacherQnt].name, name);
+    teachers[teacherQnt].sex = sex;
+    teachers[teacherQnt].birthDate = birthDate;
+    teachers[teacherQnt].cpf = cpf;
+
+    teacherQnt ++;
+
+    printf("Professor cadastrado com sucesso!\n");
+}
+#pragma endregion
+
